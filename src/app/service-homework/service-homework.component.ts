c6dc95d1248c6b1f15b3209225593febe811e6fe
wuuuudle <wuuuudle@gmail.com>
2018-10-17 23:32:12 +0800

    上传文件

--- src/app/service-homework/service-homework.component.ts
+++ src/app/service-homework/service-homework.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FileUploader} from 'ng2-file-upload';
+import {User} from '../../provider/user';
+import {Router} from '@angular/router';
+import {Api} from '../../provider/api';
+import {Holder} from '../../provider/holder';
 
 @Component({
   selector: 'app-service-homework',
@@ -12,32 +16,48 @@ export class ServiceHomeworkComponent implements OnInit {
     method: 'POST',
     itemAlias: 'file'
   });
+  loading = false;
 
-  constructor() {
+  constructor(private user: User, private router: Router, private api: Api, private holder: Holder) {
   }
 
   ngOnInit() {
   }
 
   selectedFileOnChanged(event: any) {
-    console.log(event.target.value);
-    console.log(event);
-
+    const tempfile = this.uploader.queue[this.uploader.queue.length - 1];
+    this.uploader.clearQueue();
+    this.uploader.queue.push(tempfile);
+    console.log(this.uploader.queue);
   }
 
   /**
    * 上传文件方法
    */
   uploadFile() {
+    this.loading = true;
+    const context = this;
     this.uploader.queue[0].onSuccess = function (response, status, headers) {
       if (status === 200) {
-        const tempRes = response;
-        alert(response);
+        if (response === `上传成功`) {
+          context.loading = false;
+          context.holder.alerts.push({level: 'alert-success', content: `${response}`});
+        } else if (response === '请先登录') {
+          context.holder.alerts.push({level: 'alert-danger', content: `${response}`});
+          context.router.navigateByUrl('/login');
+        } else {
+          context.holder.alerts.push({level: 'alert-danger', content: `${response}`});
+        }
       } else {
-        alert('上传失败');
+        context.holder.alerts.push({level: 'alert-danger', content: `上传失败`});
+        context.loading = false;
       }
     };
-    this.uploader.queue[0].upload(); // 开始上传
+    this.uploader.queue[0].onError = function (response, status, headers) {
+      context.holder.alerts.push({level: 'alert-danger', content: `上传失败`});
+      context.loading = false;
+    };
+    this.uploader.queue[0].upload();
   }
 
 }
